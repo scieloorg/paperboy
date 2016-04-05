f94fff8955a02e8a50c361f7435e40088f4cfc5f
fabiobatalha <fabiobatalha@gmail.com>
2016-04-05 10:45:40 -0300

    Ajustes de presets de arquivo de configuração

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
-            'send_data=paperboy:main'
+            'paperboy=paperboy:main'
         ]
     }
 )
