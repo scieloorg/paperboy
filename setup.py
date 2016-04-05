678b6ef104d7bb35e780e9b5756f6ac744270a4d
fabiobatalha <fabiobatalha@gmail.com>
2016-04-05 11:52:27 -0300

    Ajustes em script

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ tests_require = [
 
 setup(
     name="paperboy",
-    version="0.2.1",
+    version="0.2.2",
     description="Utilitário para envio de Imagens, PDF's, Traducões e Bases de sites locais SciELO para os servidores de homologação e produção",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
